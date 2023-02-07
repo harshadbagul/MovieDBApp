a9c87894f3e1d00b5369fc8d7b8544e66e3e3a1a
harshad <harshadbagul444@gmail.com>
2023-02-08 00:24:10 +0530

    Added tab fragment layout

--- app/src/main/java/com/spectrum/moviedbapp/MainActivity.kt
+++ app/src/main/java/com/spectrum/moviedbapp/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.spectrum.moviedbapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayoutMediator
+import com.spectrum.moviedbapp.databinding.ActivityMainBinding
+import com.spectrum.moviedbapp.ui.movie.ViewPagerAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    val tabArray = arrayOf(
+        "Now Playing",
+        "Popular",
+        "Top Rated",
+        "Upcoming"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val viewPager = binding.viewPager
+        val tabLayout = binding.tabLayout
+
+        val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = tabArray[position]
+        }.attach()
+
     }
 }
